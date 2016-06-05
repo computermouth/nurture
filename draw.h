174053ce78b9de4070792c31de43cba632895c51
computermouth <computermouth@crunchbangplusplus.org>
2016-06-05 03:37:34 -0700

    Fixed draw function

--- draw.h
+++ draw.h
@@ -9,22 +9,37 @@
 void draw(world* draw_world, character* draw_character){
 	
 	char buffer[10][10];
-	
-	int i;
-	for(i = 0; i < 10; i++)
-		buffer[i][10] = 0;
+	memset(buffer, 0, sizeof(buffer[0][0]) * 10 * 10);
 	
 	ushort room_ind = draw_world->map
 		[draw_character->char_world_pos->x]
 		[draw_character->char_world_pos->y];
 	
-	//printf("%d", room_ind);
 	
-	//~ int i, j;
-	//~ for(i = 0; i < 10; i++){
-		//~ for(j = 0; j < 10; j++){
-			//~ switch(draw_world->[i][j])
-		//~ }
-	//~ }
+	int i, j;
 	
+	switch(room_ind){
+		case 0:
+			break;
+		case 1:
+			for(i = 0; i < 9; i++){
+				for(j = 0; j < 9; j++){
+					if(draw_world->room_map->alpha[i][j] == 0){
+						buffer[i][j] = 'X';
+					} else {
+						buffer[i][j] = ' ';
+					}					
+				}
+			}
+			break;
+		case 2:
+			break;
+		case 9:
+			break;
+	}
+	
+	buffer[draw_character->char_room_pos->x][draw_character->char_room_pos->y] = 'A';
+	
+	for(i = 0; i < 10; i++)
+		printf("%s\n", buffer[i]);
 }
